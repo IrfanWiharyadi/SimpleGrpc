3c75e4189e2b3c11f03a25fcfedcdf646111d74a
irfanfarmaku <55388336+irfanfarmaku@users.noreply.github.com>
2020-02-27 14:46:22 +0700

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using Grpc.Net.Client;
+using GrpcService1;
+using System;
+using System.Threading.Tasks;
+using SecureRX.Services.Identity.Protos;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            //var channel = GrpcChannel.ForAddress("https://localhost:4001");
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var reply = await client.SayHelloAsync(new HelloRequest() { Name = "Tim" });
+            //Console.WriteLine(reply);
+            Console.ReadKey();
+            var channel2 = GrpcChannel.ForAddress("https://localhost:5101");
+            var client2 = new ProtoUser.ProtoUserClient(channel2);
+
+            try
+            {
+                var result = await client2.LoginAsync(new LoginRequestVM() { Application = "Web", IpAddress = "3", Username = "it@farmaku.com", Password = "P@ssw0rd", ForSpecificGroupId = 0 });
+                Console.WriteLine(result.AccessToken);
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
---
+++ GrpcService1/Services/UserServices.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using SecureRX.Services.Identity.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace GrpcService1
+{
+    public class UserServices : ProtoUser.ProtoUserBase
+    {
+        private readonly ILogger<UserServices> _logger;
+        public UserServices(ILogger<UserServices> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<LoginResponseVM> Login(LoginRequestVM request, ServerCallContext context)
+        {
+            return Task.FromResult(new LoginResponseVM
+            {
+                AccessToken = "asd"
+            });
+        }
+    }
+}
